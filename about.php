fbc48d26b2d01269b0f54049bbb106be7febae5a
safees <safeesmoha@gmail.com>
2020-10-19 16:38:38 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>
+<body>
+
+<div class="text" style="color:#000000">
+	    <div text-align="center" class="style1">About Wings Cafe</div><br>
+		<div align="justify">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Wings Cafe opened in 2009 with Drinks, burger and fatcakes. Back then, flavored chicken wings weren’t the only thing considered a rarity at Wings cafe. <br>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>Our founders were among a minority when it came to preparing popular foods like Pizza and onion rings using fresh rather than packaged ingredients. It was a lot of work, to say the least.<br />
+		</div>
+		<strong>
+	  <div align="center"><br><br><br><br>Take a “TASTE” bite of an EXCELLENT business opportunity</div></strong></div>
+
+
+</body>
+</html>
